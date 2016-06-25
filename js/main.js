e8796b04032972b3f49779e655f980e5fd23cb3a
Rob N <walrusk@gmail.com>
2016-06-24 21:05:44 -0500

    remove weather icons

--- js/main.js
+++ js/main.js
@@ -34,7 +34,6 @@ $.simpleWeather({
 	woeid: '',
 	unit: 'f',
 	success: function(weather) {
-		console.log(weather);
 		var weather_ul = $('.weather ul');
 		var forecast_days = 5;
 		if(weather.forecast.length >= forecast_days) {
