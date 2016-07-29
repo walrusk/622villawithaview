7d5d7bfd47581b94e4258abf75e38486f20151de
Rob N <walrusk@gmail.com>
2016-07-29 17:33:35 -0500

    testing email

--- _contact.php
+++ _contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // EMAIL RECIPIENT
-$to = 'toninicholson@remax.net';
+$to = 'robindnorman@gmail.com';//toninicholson@remax.net';
 $bcc = 'walrusk+thomasbooking@gmail.com';
 
 $jsonresult = array(
